abd5f6a7c5fd43a7f39ffe427e0e595dbce0cbbd
friske <minskeh@gmail.com>
2026-02-15 23:15:49 +0900

    テンプレート作成

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/books')
-def main():
-    return "Hello, World!"
+def books():
+    return render_template('books/index.html')
+
+@app.route('/books/new')
+def new_book():
+    return render_template('books/new/registration.html')
+
+@app.route('/books/<int:book_id>/edit')
+def edit_book(book_id):
+    return render_template('books/edit/edit.html')
+
+@app.route('/books/<int:book_id>/delete')
+def delete_book(book_id):
+    return render_template('books/delete/delete.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
